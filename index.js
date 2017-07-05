f339fbcdc9573856383130de272b9933b2c13c70
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-07-05 11:04:41 -0700

    header style and js

---
+++ index.js
@@ -0,0 +1,12 @@
+var date = new Date();
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+$('.todo--weekday').text(function() {
+  return days[date.getDay()];
+});
+
+$('.todo--fulldate').text(function() {
+  return monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+});
+
